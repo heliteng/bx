231c3e2e8a43bc48241f1b04907d3a0d2f058df4
heliteng <185656746@qq.com>
2025-02-21 20:31:59 +0800

    update

---
+++ docs/.vitepress/view/user-agreement.vue
@@ -0,0 +1,141 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <div class="box">
+
+    <p align=center ><b style='font-weight: bold;font-size: 30pt'>   用户协议</b><b style='mso-bidi-font-weight:
+		normal'><span lang=EN-US style='font-size:16.0pt;line-height:150%;font-family:
+		宋体;mso-bidi-font-family:宋体;background:white'></span></b></p>
+
+    <p class=MsoNormal align=right style='margin-right:5.25pt;mso-para-margin-right:
+		.5gd;text-align:right;text-indent:21.0pt;mso-char-indent-count:1.75;line-height:
+		150%'><span style='font-size:12.0pt;line-height:150%;font-family:宋体;mso-bidi-font-family:
+		宋体;mso-font-kerning:0pt;mso-bidi-language:AR;mso-bidi-font-weight:bold'>更新日期：<span
+        lang=EN-US>2025</span>年<span lang=EN-US>2</span>月<span lang=EN-US>12</span>日<span
+        lang=EN-US></span></span></p>
+
+    <p class=MsoNormal align=right style='margin-right:5.25pt;mso-para-margin-right:
+		.5gd;text-align:right;text-indent:21.0pt;mso-char-indent-count:1.75;line-height:
+		150%'><span style='font-size:12.0pt;line-height:150%;font-family:宋体;mso-bidi-font-family:
+		宋体;mso-font-kerning:0pt;mso-bidi-language:AR;mso-bidi-font-weight:bold'>生效日期：<span
+        lang=EN-US>2025</span>年<span lang=EN-US>2</span>月<span lang=EN-US>12</span>日<span
+        lang=EN-US></span></span></p>
+    
+    <section>
+      <p>“易边学”APP（下称本应用）用户分为注册用户和非注册用户。注册用户是指根据本应用注册页面提示信息进行注册成功后的用户，需要使用注册的账号及设置的密码（账号及密码合称“账户”）登录本应用。
+        非注册用户是指无须进行注册即可使用本应用部分服务的用户。注册用户和非注册用户因其在平台形成的信息、使用的服务的范围和内容不同，具有不同的权利和义务。</p>
+    </section>
+
+    <section>
+      <h1>一、涉及注册用户的声明</h1>
+      <p>本部分中，所表述的“用户”均指“注册用户”。</p>
+      <h2>（一）服务内容</h2>
+      <p class="indent">1、本应用向用户提供的服务包括：
+        英语学习服务、少儿守护服务、以及本应用为用户提供的其他服务。</p>
+      <p class="indent">2、用户使用本应用提供的具体服务时，应当满足相应的前提条件、具备相应的服务使用权限。用户不满足使用服务的前提条件、不具备服务使用权限的，无法使用相应服务。</p>
+      <p class="indent">3．我方有权因网络维护、更新等原因中断向用户提供相关服务，待维护、更新完成后继续向用户提供服务。</p>
+      <h2>（二）账户的注册与使用</h2>
+      <p class="indent">1．请您根据本应用注册页面提示信息进行注册，注册成功后，您有权使用您注册的账户登录本应用。</p>
+      <p class="indent">2．您向平台提供的联系方式如发生变更，您应当及时在平台更新信息。如果因您提供的联系方式不准确，或您未及时更新您的联系方式信息，您应当自行承担因此而产生的不利后果。</p>
+      <p class="indent">3．您了解并同意：您通过本应用使用图片转文字功能时，本应用有权对您上传的图片进行识别、存储，并返回识别结果。</p>
+      <h2>（四）账户的安全及管理</h2>
+      <p class="indent">1．您的账户遭到未经授权的使用时，您应当立即通知本应用，否则未经授权的使用行为均视为您本人的行为，您将自行承担所有由此导致的损失及后果。</p>
+      <p class="indent">2．您应对您账户项下的所有行为结果负责。如发现任何未经授权使用您账户登录本应用或其他可能导致您账户遭窃、遗失的情况，建议您立即通知我方。您理解我方对您的任何请求采取行动均需要合理时间，除我方存在过错外，我方对在采取行动前已经产生的后果不承担任何责任。</p>
+      <p class="indent">3．我方有权基于单方独立判断，在认为可能不利于本应用提供服务或涉及违法违规等情形时，可不经通知而先行暂停、中断或终止向您提供本协议项下的全部或部分服务，对此我方不承担任何责任。</p>
+      <p class="indent">前述情形包括但不限于：</p>
+      <p class="indent">(1) 本应用认为您未进行注册或者提供的注册资料不真实的；</p>
+      <p class="indent">(2) 本应用认为您不是登录账户的信息权利人或存在未经授权的使用时；</p>
+      <p class="indent">(3) 本应用发现异常情况或有合理理由怀疑操作有风险或有违法之虞时；</p>
+      <p class="indent">(4) 本应用认为您已经违反本协议约定的。</p>
+      <h2>（五）免责声明</h2>
+      <p class="indent">1．您理解并同意，本应用对以下情形不做任何担保，包括但不仅限于：</p>
+      <p class="indent">(1) 服务一定能满足您的要求；</p>
+      <p class="indent">(2) 服务不会受中断；</p>
+      <p class="indent">(3) 服务的安全性、及时性、完整性和准确性；</p>
+      <p class="indent">(4) 服务所涉及相关方的服务；</p>
+      <p class="indent">(5) 您收到的来自本应用的口头或书面的意见或信息；</p>
+      <p class="indent">(6) 您所提供的身份信息一定被注册；</p>
+      <p class="indent">(7) 他人无法使用与您相同的身份信息注册。</p>
+    </section>
+
+    <section>
+      <h1>二、涉及非注册用户的声明</h1>
+      <p>本部分中，所表述的“用户”均指“非注册用户”。为保障信息安全，对非注册用户不提供相关服务。</p>
+      <h2>（一）关于搜集、分拣信息</h2>
+      <p class="indent">当您浏览或阅读本应用的信息时，平台会自动搜集和分拣到您的信息（包括但不限于访问时间、访问页面），这些信息不会被用来确定您的身份。</p>
+      <h2>（二）关于信息安全</h2>
+      <p class="indent">本应用将使用相应的技术，对您所提供的个人资料进行严格的管理和保护，防止个人资料丢失、被盗用或遭篡改。</p>
+      <p class="indent">本应用只有在下列条件下，方对收集的个人资料进行必要范围以外的使用：</p>
+      <p class="indent">(1) 已取得您的书面同意；</p>
+      <p class="indent">(2) 为免除您在生命、身体或财产方面遇到的急迫危险；</p>
+      <p class="indent">(3) 为防止他人权益遭受重大危害；</p>
+      <p class="indent">(4) 为增进公共利益，且无损于您的重大利益。</p>
+      <p class="indent">(5) 当政府机关依照法定程序要求本应用披露个人资料时，本应用将根据其要求或为了公共安全的目的提供个人资料。</p>
+      <p class="indent">(6) 经我方独立判断后需要进行必要范围以外的使用的其他条件。</p>
+    </section>
+
+    <section>
+      <h1>三、公共声明</h1>
+      <p>本部分声明对注册用户和非注册用户均适用。</p>
+      <h2>（一）免责声明</h2>
+      <p class="indent">1．您理解并同意，因下列原因导致本应用无法正常提供服务的，本应用不承担责任：</p>
+      <p class="indent">(1) 本应用及第三方机构等承载本服务系统停机维护期间；</p>
+      <p class="indent">(2) 您的手机软件和通信线路、供电线路出现故障的；</p>
+      <p class="indent">(3) 您操作不当或通过非本应用及第三方机构授权或认可的方式使用服务的；</p>
+      <p class="indent">(4) 因病毒、木马、恶意程序攻击、网络拥堵、系统不稳定、系统或设备故障、通讯故障、电力故障或政府行为等原因；</p>
+      <p class="indent">(5) 由于黑客攻击、网络供应商技术调整或故障、平台升级系统方面的问题等原因而造成的服务中断或延迟；</p>
+      <p class="indent">(6) 因台风、地震、海啸、洪水、停电、战争、恐怖袭击等不可抗力之因素，造成本服务系统障碍不能执行业务的。</p>
+      <p class="indent">2．发生下列情况时本应用不承担任何责任：</p>
+      <p class="indent">(1) 本应用已采取必要的信息安全保护措施但仍遭受到黑客攻击、计算机病毒侵入或发作，因政府部门管制和其他不可抗力等，造成的个人资料泄露、丢失、被盗用或被篡改等。</p>
+      <p class="indent">(2) 本应用不能控制或保证与之相链接的各个网站信息的准确性、及时性和完整性，因与本应用链接的其它网站造成个人资料泄露及由此而导致的任何法律争议和后果。</p>
+      <h2>（二）特别约定</h2>
+      <p class="indent">1．您在本应用的行为若有任何违反国家法律法规或侵犯任何第三方的合法权益的情形时，我方有权直接删除该等违反规定之信息，并可以暂停或终止服务。</p>
+      <p class="indent">2．若您利用本应用从事任何违法或侵权行为，由您自行承担全部责任，并承担因此给我方或任何第三方造成的全部损失。</p>
+      <h2>（三）条款有效性</h2>
+      <p class="indent">本协议中的任何条款无论因何种原因完全或部分无效或不具有执行力，则应认为该条款可与本协议相分割。在此情况下，本协议的其他条款仍然完全有效并具有约束力。</p>
+      <h2>（四）隐私与数据保护</h2>
+      <p class="indent">为提供学习及护眼功能，本应用将收集必要信息（如设备信息、学习记录等），详见<a target="_blank" href="https://oaak.netlify.app/privacy-policy.html">《隐私政策》</a>。</p>
+      <p class="indent">儿童用户信息需经监护人同意后处理，家长可通过“少儿守护”功能管理内容与使用时长。</p>
+
+      <h2>（五）法律适用</h2>
+      <p class="indent" style="margin-bottom: 100px">本协议之生效、解释、补充、终止等均适用中华人民共和国法律（大陆地区）。</p>
+    </section>
+  </div>
+
+</template>
+
+<style scoped>
+* {
+  color: #000000;
+}
+.box{
+  padding: 50px 10px 50px 10px;
+}
+
+p {
+  margin-top: 15px;
+}
+section {
+  margin-top: 21pt;
+}
+h1 {
+  font-size: 14pt;
+  font-weight: bold;
+}
+h1, h2, h3 {
+  margin-top: 21pt;
+  margin-bottom: 15pt;
+}
+p {
+  text-indent: 21pt;
+}
+.indent {
+  text-indent: 24pt;
+}
+.bold {
+  font-weight: bold;
+}
+</style>
---
+++ docs/user-agreement.md
@@ -0,0 +1,18 @@
+---
+sidebar: false
+navbar: false
+footer: false
+lastUpdated: false
+aside: false
+editLink: false
+prev: false
+next: false
+layout: page
+---
+
+
+<script setup>
+import privacyPolicy from '.vitepress/view/user-agreement.vue'
+</script>
+
+<privacyPolicy />
